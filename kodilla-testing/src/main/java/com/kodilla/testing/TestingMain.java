d20202c0b9af6b5d3c06b440d4f4f3ebfd48acc3
Patryk Sobow <patryk.sobow95@gmail.com>
2018-08-08 19:38:13 +0200

    Stage 6.2 - Unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        final int a = 2, b = 2;
+        Calculator myCalculator = new Calculator();
+
+        int result = myCalculator.add(a, b);
+
+        if (result == (a+b) ) {
+            System.out.println("Test dodawania poprawny");
+        }
+        else System.out.println("Test dodawania niepoprawny");
+
+        result = myCalculator.subtract(a, b);
+
+        if (result == (a-b) ) {
+            System.out.println("Test odejmowania poprawny");
+        }
+        else System.out.println("Test odejmowania niepoprawny");
+
+
     }
 }
