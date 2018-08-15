2c4252d11a252b7f0e5a14a7d4459929bf884b0d
Patryk Sobow <patryk.sobow95@gmail.com>
2018-08-15 17:32:32 +0200

    Stage 7.2 - Stream, first time

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -44,5 +45,9 @@ public class StreamMain {
 
         poemBeautifier.beautifyAndShow("Welcome", reverseString);
         //poemBeautifier.beautifyAndShow("Welcome", reverseString.toUpperCase());
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
