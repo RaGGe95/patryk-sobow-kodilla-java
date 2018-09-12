c2b893d138757b64927271e4daea0ef731573036
Patryk Sobow <patryk.sobow95@gmail.com>
2018-09-12 23:00:32 +0200

    Stage 8.4 - Recursion searching MULTI-THREAD, only for tree data-struct

--- kodilla-exception/src/main/java/com/kodilla/exception/recursion/FlightSeeker.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/recursion/FlightSeeker.java
@@ -2,89 +2,42 @@ package com.kodilla.exception.recursion;
 
 
 
-        /* to jest drzewo
-        Warszawa -> Berlin, Paryż, Lodyn
-        Berlin -> Eigpt, Moskwa
-        Moskwa -> Tokio
-        Tokio -> Nowy York
-         */
-
-
 public class FlightSeeker {
-    private boolean searchResult = false;
 
     public FlightSeeker() {}
 
 
     // Łatwiejszy algorytm. dla drzewa.
     public boolean isFlightPossibleForTreeStructure(Airport start, Airport finish) {
-        searchResult = false;
+        if (start.getConnections() == null || start.getConnections().size() == 0) {
+            return false;
+        }
 
         if (start.getConnections().contains(finish)) {
-            searchResult = true;
-            return searchResult;
+            return true;
         }
 
         for (Airport each : start.getConnections()) {
-            if (searchResult) break;
-
-            if (each.getConnections().size() != 0) {
-                isFlightPossibleForTreeStructure(each, finish);
+            if(isFlightPossibleForTreeStructure(each, finish)) {
+                return true;
             }
         }
-        return searchResult;
+        return false;
     }
 
 
 
+    /*  to jest drzewo
+        Warszawa -> Berlin, Paryż, Lodyn
+        Berlin -> Eigpt, Moskwa
+        Moskwa -> Tokio
+        Tokio -> Nowy York
+        */
 
      /* to jest graf (tu są powtórzenia)
         Warszawa -> Berlin, Paryż, Lodyn
         Berlin -> Warszawa, Eigpt, Moskwa
         Moskwa -> Berlin, Tokio
         Tokio -> Moskwa, Nowy York
-
-
-        // Algorytm w którym musimy wyeliminować nieskońćzone pętle
-    // Berlin, newyork
-        public boolean isFlightPossibleForGraphStructure(Airport start, Airport finish) throws RouteNotFoundException {
-
-            if (set.contains(finish)) {
-                set.clear();
-                return true; // znaleźliśmy nasze docelowe lotnisko gdzieś przed końcem drzewa!
-            }
-
-
-            Iterator<Airport> iter = start.getConnections().iterator();
-
-            while (iter.hasNext()) {
-
-                Airport currentAirport = iter.next();
-
-                if( set.contains(currentAirport) )
-                    continue;
-
-
-                set.add(currentAirport);
-
-
-
-                if (  currentAirport.getConnections().size() != 0   ) {
-                    isFlightPossibleForGraphStructure(currentAirport, finish);
-                }
-
-            }
-
-
-
-            if (! set.contains(finish)) {
-                set.clear();
-                throw new RouteNotFoundException("Airport has not been found. ");
-            } else {
-                set.clear();
-                return true;
-            }
-        }
-*/
-
+    */
 }
