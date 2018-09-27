449cac4ac02f90ff2138200a758d2acdaec52bdd
Patryk Sobow <patryk.sobow95@gmail.com>
2018-09-27 18:12:56 +0200

    Stage 13.1 - Hibernate setup

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,12 +1,12 @@
-package com.kodilla;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaCourseApplication {
+public class KodillaHibernateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KodillaCourseApplication.class, args);
+        SpringApplication.run(KodillaHibernateApplication.class, args);
     }
 }
